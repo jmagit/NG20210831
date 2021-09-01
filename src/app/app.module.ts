848ee3f40ac7b2aec9798282adb442509cc8e7e8
Javier <jmartin@grupoloyal.com>
2021-09-01 19:13:51 +0200

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { MyCoreModule } from 'src/my-core';
+import { LoggerService, MyCoreModule } from 'src/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MainModule } from './main';
@@ -19,7 +19,9 @@ import { AppComponent } from './app.component';
     CommonModule, MainModule, SecurityModule, MyCoreModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LoggerService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
